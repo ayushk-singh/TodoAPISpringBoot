fee4a6544959ca42257e3845e7cb53a3317c6d41
Ayush <aayushsingh.code@gmail.com>
2026-04-22 18:01:56 +0530

    clean start

---
+++ src/test/java/xyz/softwareguy/TodoApp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.softwareguy.TodoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
